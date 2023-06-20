56915ade8da6bcf7e5261cde80ce90798280580c
petr.magnusek@t-mobile.cz <petr.magnusek@t-mobile.cz>
2023-06-20 15:15:49 +0200

    cviceni 1 done

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,12 +2,28 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
 
-        Lemonade lem1 = new Lemonade("fanta", "pomerancova", 0.7);
-        lem1.openBottle();
-        //lem1.setFlavor("jahodova");
-        System.out.println(lem1.getFlavor());
+        // prvni cast
+        Car car = new Car("skoda", "fabia", 2015);
 
+        prnValues(car);
+
+        car.setMake("Ford");
+        car.setModel("Focus");
+        car.setYear(2010);
+
+        prnValues(car);
+
+        // druha cast
+        Rectangle rectangle = new Rectangle(12, 20);
+        double area = rectangle.getArea();
+        System.out.println("Obsah je: " + area);
+
+    }
+
+    private static void prnValues(Car car) {
+        System.out.println("maker is: " + car.getMake());
+        System.out.println("model is: " + car.getModel());
+        System.out.println("year is: " + car.getYear());
     }
 }
---
+++ src/main/java/org/example/Rectangle.java
@@ -0,0 +1,37 @@
+package org.example;
+
+public class Rectangle {
+
+    private double length;
+    private double width;
+
+    public Rectangle(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    public double getLength() {
+        return this.length;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+    public double getWidth() {
+        return this.width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public double getArea() {
+        return this.width * this.length;
+    }
+
+    public double getPerimeter() {
+        return this.length / 2;
+    }
+
+}
